6c359543f1fa14f54e30e5123b7a560401a15666
eric1221bday <efang@andrew.cmu.edu>
2017-04-19 03:37:09 -0400

    Initial Commit

---
+++ PGH Bus Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PGH Bus Tracker
+//
+//  Created by Eric Fang on 4/19/17.
+//  Copyright © 2017 Eric Fang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PGH Bus TrackerTests/PGH_Bus_TrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  PGH_Bus_TrackerTests.swift
+//  PGH Bus TrackerTests
+//
+//  Created by Eric Fang on 4/19/17.
+//  Copyright © 2017 Eric Fang. All rights reserved.
+//
+
+import XCTest
+@testable import PGH_Bus_Tracker
+
+class PGH_Bus_TrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
